988bbaa04ac44b37ac0231601185a6fc3f8e703a
petemoyaplg <gaspardplg11@gmail.com>
2021-05-03 14:51:49 +0100

    projet allo-ciné initialisé

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import "../style/style.css"
+import Preloader from "./Preloader/Preloader"
+
+const App = () => {
+  return (
+    <div>
+      <Preloader />
+    </div>
+  );
+}
+
+export default App;
